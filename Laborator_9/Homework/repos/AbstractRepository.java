66746dd7cf7c7b953b9e3bb20fab1b955ca8018f
claudiuchichirau <100518779+claudiuchichirau@users.noreply.github.com>
2023-05-10 10:01:06 +0300

    Add files via upload

---
+++ Laborator_9/Homework/repos/AbstractRepository.java
@@ -0,0 +1,23 @@
+package org.example.repos;
+
+
+import jakarta.persistence.EntityManager;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+@NoRepositoryBean
+public interface AbstractRepository<T, ID extends Serializable> extends CrudRepository<T, ID> {
+
+    List<T> findByName(String name);
+    boolean persist(T entity);
+    void setEntityManager(EntityManager entityManager);
+    EntityManager getEntityManager();
+    void rollback();
+    void handleException(Exception exception);
+    void commit();
+    void beginTransaction();
+
+}
